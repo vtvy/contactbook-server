3134fd85485c7646de4c5ba3ca589cfcd6dd9a2a
vtvy <votrieuvy1661@gmail.com>
2022-09-16 09:10:09 +0700

    Implement an error handling middleware

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-//start server
+// start server
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const contactController = require("./controllers/contact.controller");
+const ApiError = require("./api-error");
+const { restart } = require("nodemon");
 
 const app = express();
 
@@ -23,5 +25,22 @@ app
   .get(contactController.findOne)
   .put(contactController.update)
   .delete(contactController.delete);
+
+// Handle 404 response.
+app.use((req, res, next) => {
+  // Handle for unknown route
+  //      Call next() to pass to the error handling middleware
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Define error-handling middleware last, after other app.use() and routes calls.
+app.use((error, req, res, next) => {
+  // The centralized error handing middleware.
+  // In any route handler, calling next(error)
+  //      will pass to this error hanling middleware.
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
