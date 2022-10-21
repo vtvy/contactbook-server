4528a6af8ba3487b619538fea61b524b5549ad16
vtvy <votrieuvy1661@gmail.com>
2022-10-21 10:08:47 +0700

    edit PORT and add Migration file

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 
 // start server
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/database/migration.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE ct313h_labs;
+CREATE TABLE `contacts` (
+`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci',
+`email` TEXT NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+`address` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_general_ci',
+`phone` TINYTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+`favorite` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB;
